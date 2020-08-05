d45c7fd15b464e2aedbc1df3a4ee6adbb87e661d
ChristyTropila <tropila04@gmail.com>
2020-08-05 15:50:59 -0400

    Object class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+  
+@@all =[]
+
+
+  def initialize (name_arg)
+    @name=name_arg
+    @species="human"
+    @@all << self
+
+  end
+
+  def say_species
+"I am a #{@species}."
+  end
+
+  def self.all
+   @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear # reset
+  end
+
+
 end
